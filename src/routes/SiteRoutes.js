8f577bea8ceca9019e307816e1f9860d6fa3e684
Jose Alejandro <alejandro.sa26@gmail.com>
2019-08-16 07:20:01 -0400

    New route

--- src/routes/SiteRoutes.js
+++ src/routes/SiteRoutes.js
@@ -3,6 +3,8 @@ import { Switch, Route } from 'react-router-dom';
 import Header from '../components/Header';
 import SignIn from '../components/SignIn';
 import SignUp from '../components/SignUp';
+import BodyTickets from '../components/BodyTickets'; 
+import BodyAdminTickets from '../components/BodyAdminTickets'; 
 // body content components
 
 class SiteRoutes extends React.Component {
@@ -20,6 +22,22 @@ class SiteRoutes extends React.Component {
           return (
             <div id='full-site-wrapper'>
               <SignUp/>
+            </div>
+          );
+        }}/>
+        <Route exact path='/tickets' render={(props) => {
+          return (
+            <div id='full-site-wrapper'>
+              <Header/>
+              <BodyTickets/>
+            </div>
+          );
+        }}/>
+        <Route exact path='/admin' render={(props) => {
+          return (
+            <div id='full-site-wrapper'>
+              <Header/>
+              <BodyAdminTickets/>
             </div>
           );
         }}/>
